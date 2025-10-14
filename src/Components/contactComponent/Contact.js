6cf851fb79259cc2b604b09002095b6c595e58cf
khuhsal2504 <nikamkhushal010@gmail.com>
2025-10-14 20:41:40 +0530

    your comment

--- src/Components/contactComponent/Contact.js
+++ src/Components/contactComponent/Contact.js
@@ -17,11 +17,11 @@ export default function Contact() {
                     <input type="text" className="form-control" id="username"
                 {...register('username',{required:true,minLength:3, maxLength:10})}/>
                 <div className="form-text text-danger">
-                    {formState.errors.username && formState.errors.username.type=='required' && "UserName is required"}
+                    {formState.errors.username && formState.errors.username.type==='required' && "UserName is required"}
 
-                    {formState.errors.username && formState.errors.username.type=='minLength' && "Min 3 characters required"}
+                    {formState.errors.username && formState.errors.username.type==='minLength' && "Min 3 characters required"}
 
-                    {formState.errors.username && formState.errors.username.type=='maxLength' && "Max 10 characters allowed"}
+                    {formState.errors.username && formState.errors.username.type==='maxLength' && "Max 10 characters allowed"}
                 </div>
                 </div>
                 <div className="mb-3">
@@ -29,11 +29,11 @@ export default function Contact() {
                 <input type="number" className="form-control" id="userage"
                 {...register('userage',{required:true,min:18, max:30})}/>
                 <div className="form-text text-danger">
-                    {formState.errors.userage && formState.errors.userage.type=='required' && "UserAge is required"}
+                    {formState.errors.userage && formState.errors.userage.type==='required' && "UserAge is required"}
 
-                    {formState.errors.userage && formState.errors.userage.type=='min' && "Min age required is 18"}
+                    {formState.errors.userage && formState.errors.userage.type==='min' && "Min age required is 18"}
 
-                    {formState.errors.userage && formState.errors.userage.type=='max' && "Max age required is 30"}
+                    {formState.errors.userage && formState.errors.userage.type==='max' && "Max age required is 30"}
 
                 </div>
                 </div>
@@ -41,14 +41,14 @@ export default function Contact() {
                 <label htmlFor="password1" className="form-label">Password</label>
                 <input type="password" className="form-control" id="password1" {...register('password1',{required:true})}/>
                 <div className="form-text text-danger">
-                {formState.errors.password1 && formState.errors.password1.type=='required' && "Password is required"}
+                {formState.errors.password1 && formState.errors.password1.type==='required' && "Password is required"}
                 </div>
                 </div>
                 <div className="mb-3">
                 <label htmlFor="password2" className="form-label">Confirm Password</label>
                 <input type="password" className="form-control" id="password2" {...register('password2',{required:true, validate: value => value === getValues('password1') || "Passwords do not match"})}/>
                 <div className="form-text text-danger">
-                {formState.errors.password2 && formState.errors.password2.type=='required' && "Confirm Password is required"}
+                {formState.errors.password2 && formState.errors.password2.type==='required' && "Confirm Password is required"}
                 {formState.errors.password2 && formState.errors.password2.message}
                 </div>
                 </div>
